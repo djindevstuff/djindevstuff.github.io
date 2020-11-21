0ebf3182f2f14d615335b0e7150379f6b49edadd
Adil Ansari <adila25122001@gmail.com>
2020-11-21 17:02:31 +0530

    Seperated CSS & JS part from HTML files.

--- crystals/Scripts/champPull.js
+++ crystals/Scripts/champPull.js
@@ -16,7 +16,7 @@ function readFile(file){
     f.send(null);
 }
 function readFi(){
-    readFile("champs.txt");
+    readFile("../res/champs.txt");
 }
 function randomInt(min,max){
     return Math.floor(Math.random()*(max-min+1))+min;
