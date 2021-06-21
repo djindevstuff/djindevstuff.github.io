ae0bf27ec53c416b9a761e0c337b018b9c137bd2
Adil <73538455+djindevstuff@users.noreply.github.com>
2021-06-21 17:47:16 +0530

    Updated!

--- crystals/nexfivestar/nexfivestar.js
+++ crystals/nexfivestar/nexfivestar.js
@@ -4,52 +4,21 @@ var champNex=[3];
 var r=[3];
 var res;
 var hist="";
-var cloudinary="https://res.cloudinary.com/dtpybxseq/image/upload/mcoc_pfp/";
+var cloudinary="https://res.cloudinary.com/dtpybxseq/image/upload/pfp_mcoc/";
 var git="https://djindevstuff.github.io/crystals/";
 
-function readFile(file){
-    var f = new XMLHttpRequest();
-    f.open("GET", file, true);
-    f.onreadystatechange = function (){
-        if(f.readyState === 4){
-            if(f.status === 200 || f.status == 0){
-                res= f.responseText;
-                champ=res.split("\n");
-            }
-        }
-    }
-    f.send(null);
-}
-function read5Star(){
-    readFile(git+"pool/fivestarbasic.txt");
-}
-function randomInt(min,max){
-    return Math.floor(Math.random()*(max-min+1))+min;
-}
-function getChamp(len){
-    r[0]=randomInt(0,len);
-    r[1]=randomInt(0,len);
-    r[2]=randomInt(0,len);
-    while(r[0]==r[1]){
-        r[1]=randomInt(0,len);
-    }
-    while(r[2]==r[0] || r[2]==r[1]){
-        r[2]=randomInt(0,len);
-    }
-    champNex[0] = champ[r[0]];
-    champNex[1] = champ[r[1]];
-    champNex[2] = champ[r[2]]; 
-}
-function open1Crystal(n){
+
+function openNexFiveCrystal(n){
     for(var i=0;i<n;i++){
         totalCount+=1;
         hist=document.getElementById("showChamp").innerHTML;
         document.getElementById("showChamp2").innerHTML=hist+"<br>"+document.getElementById("showChamp2").innerHTML;
-        getChamp(champ.length-1);
-        document.getElementById("showChamp").innerHTML = "<img class=\"border\" src=\""+git+"featfivepool/frame5.png\" width=\"72px\" height=\"60px\"><img class=\"fivestar\" src=\""+cloudinary+champNex[0]+"\"><img class=\"border\" src=\""+git+"featfivepool/frame5.png\" width=\"72px\" height=\"60px\"><img class=\"fivestar\" src=\""+cloudinary+champNex[1]+"\"><img class=\"border\" src=\""+git+"featfivepool/frame5.png\" width=\"72px\" height=\"60px\"><img class=\"fivestar\" src=\""+cloudinary+champNex[2]+"\"><br>-------------------";
+        var nexchamp = get3NexFiveChamp();
+        document.getElementById("showChamp").innerHTML = "<img class=\"border\" src=\""+cloudinary+"frame5.png\" width=\"72px\" height=\"60px\"><img class=\"fivestar\" src=\""+cloudinary+nexchamp[0]+".png\"><img class=\"border\" src=\""+cloudinary+"frame5.png\" width=\"72px\" height=\"60px\"><img class=\"fivestar\" src=\""+cloudinary+nexchamp[1]+".png\"><img class=\"border\" src=\""+cloudinary+"frame5.png\" width=\"72px\" height=\"60px\"><img class=\"fivestar\" src=\""+cloudinary+nexchamp[2]+".png\"><br>-------------------";
     }
     document.getElementById("countTotal").innerHTML = totalCount;
 }
+
 function clearWindow(){
     totalCount=0;
     document.getElementById("countTotal").innerHTML=0;
---
+++ crystals/script/opener.js
@@ -0,0 +1,58 @@
+var champ;
+var x;
+var nex=[3];
+
+function readjson(level){
+    x=level;
+    let requestURL = '..\\poolmaker\\poolinfo.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        champ = request.response;
+        //champ=superHeroes;
+        //console.log(champ['fivestarbasicpool'].length);
+        //console.log(superHeroes['fivestarbasicpool'][5]);
+        //populateHeader(superHeroes);
+        //showHeroes(superHeroes);
+        //makeArray2(superHeroes);
+      }
+}
+
+function randomInt(min,max){
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+function getFiveStarChamp(){
+    var r=randomInt(0,champ['fivestarbasicpool'].length-1);
+    return champ['fivestarbasicpool'][r];
+}
+
+function getSixStarChamp(){
+    var r=randomInt(0,champ['sixstarbasicpool'].length-1);
+    return champ['sixstarbasicpool'][r];
+}
+
+function getChamp(len){
+    nex[0]=randomInt(0,len);
+    nex[1]=randomInt(0,len);
+    nex[2]=randomInt(0,len);
+    while(nex[0]==nex[1]){
+        nex[1]=randomInt(0,len);
+    }
+    while(nex[2]==nex[0] || nex[2]==nex[1]){
+        nex[2]=randomInt(0,len);
+    }
+}
+
+function get3NexFiveChamp(){
+    getChamp(champ['fivestarbasicpool'].length-1);
+    var nexpool=[champ['fivestarbasicpool'][nex[0]], champ['fivestarbasicpool'][nex[1]],champ['fivestarbasicpool'][nex[2]]];
+    return nexpool;
+}
+function get3NexSixChamp(){
+    getChamp(champ['sixstarbasicpool'].length-1);
+    var nexpool=[champ['sixstarbasicpool'][nex[0]], champ['sixstarbasicpool'][nex[1]],champ['sixstarbasicpool'][nex[2]]];
+    return nexpool;
+}
